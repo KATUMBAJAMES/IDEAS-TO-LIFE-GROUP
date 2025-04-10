422293331c35ba2aaa344073154dec42e57c3a3c
KATUMBAJAMES <katumbalexat2020@gmail.com>
2025-04-10 11:06:04 +0300

    Add files via upload

---
+++ sacco/CreateAccount.java
@@ -0,0 +1,10 @@
+public class CreateAccount{
+    //account details
+    String first_name;
+    String sec_name;
+    String tel;
+    String NIN;
+    String dob;
+    String sex;
+    String nextOfKin;
+}
---
+++ sacco/Main.java
@@ -0,0 +1,33 @@
+/*
+ 
+*/
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args){
+        //System.out.println("hello there");
+        //options 
+        System.out.println("Please select from menu.");
+        System.out.println("1. Create account");
+        System.out.println("2. Login");
+        
+        Scanner input = new Scanner(System.in);
+
+        int option;
+
+        System.out.println("Enter : ");
+
+        option = input.nextInt();
+
+        switch(option){ 
+            case 1:{
+                //login function/class
+                break;
+            }
+            case 2:{
+                //login fuction/class
+            }
+        }
+    }
+}
